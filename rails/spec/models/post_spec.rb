5e1fb1c5b6b3e9820dc236b3510f9a59aae0de32
neopuri0224 <neopuri0224@gmail.com>
2020-12-04 20:54:46 +0900

    :+1: add post validation

--- rails/spec/models/post_spec.rb
+++ rails/spec/models/post_spec.rb
@@ -6,5 +6,13 @@ RSpec.describe Post, type: :model do
       subject { create(:post, title: "これはテストです", description: "これはテストです") }
       it { is_expected.to be_valid }
     end
+
+    context "when save invalid data" do
+      let!(:post) { Post.new() }
+      it 'in valid title' do
+        post.valid?
+        expect(post.errors.messages[:title]).to include("can't be blank")
+      end
+    end
   end
 end
