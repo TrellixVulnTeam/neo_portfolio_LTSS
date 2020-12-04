f9d7cda8349b26c6814d0f6dd6b5d2253b8c4870
neopuri0224 <neopuri0224@gmail.com>
2020-12-04 21:15:02 +0900

    :+1: posts controller実装

---
+++ rails/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,10 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+      def index
+        posts = Post.all
+        render json: { status: 'SUCCESS', data: posts }
+      end
+    end
+  end
+end
